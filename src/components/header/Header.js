a46a56364095a83d1c3452983f517fba106e4fc1
Yusuf Alp <alpyusuf007@gmail.com>
2021-02-14 13:01:12 -0800

    add navigation links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css';
-
 import { Link } from 'react-router-dom';
+import GoogleAuth from '../auth/GoogleAuth';
 
 const Header = () => {
   return (
     <header className="App-header">
       <ul>
-        <li><Link to="/" className="home-link">Ripplers</Link></li>
-        <li><Link to="/" className="menu-link">Streams</Link></li>
-        <li><Link to="/" className="menu-link">Login</Link></li>
+        <li className="home-link"><Link to="/">Ripplers</Link></li>
+        <li className="menu-link"><Link to="/">Streams</Link></li>
+        <li className="menu-link"><Link to="/">Login</Link></li>
+        <li className="menu-link"><GoogleAuth /></li>
       </ul>
     </header>
   );

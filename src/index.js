9f75e0fcd1daf118a02e95f85ede6c2a15dc8072
Yusuf Alp <alpyusuf007@gmail.com>
2021-02-15 12:48:57 -0800

    use Provider and create store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );

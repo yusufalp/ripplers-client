8b8810e5ec392eb188ba58d76e3f6660db9dfff5
Yusuf Alp <alpyusuf007@gmail.com>
2021-02-17 09:04:51 -0800

    implement a redux form with two fields

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
-};
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    return (
+      <div>
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
+      </div>
+    );
+  }
 
-export default StreamCreate;
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);

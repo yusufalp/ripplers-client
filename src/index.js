7d0a56d375a8ca2dde48fd447082ccfe244c9891
Yusuf Alp <alpyusuf007@gmail.com>
2021-02-17 08:29:54 -0800

    install redux devtools chrome extenrions and connect it to the app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './components/App/App';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware())
+);
 
 ReactDOM.render(
   <Provider store={store}>

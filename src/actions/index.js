be24f9c84a4731d6cf43001db4ae8ecdb9c0a2ef
Yusuf Alp <alpyusuf007@gmail.com>
2021-02-25 06:42:04 -0800

    add a create-stream action and a type for the action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../apis/streams';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
 
 export const signIn = (userId) => {
   return {
@@ -12,3 +13,10 @@ export const signOut = () => {
     type: SIGN_OUT
   };
 };
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    const response = await streams.post('/streams', formValues);
+    dispatch({ type: CREATE_STREAM, payload: response.data })
+  }
+}

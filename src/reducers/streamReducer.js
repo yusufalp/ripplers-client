4a3e352730dc5236fca36f803400acd519e06714
Yusuf Alp <alpyusuf007@gmail.com>
2021-02-25 07:40:09 -0800

    create reducers for all action types defined for streams

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import {
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      // mapKeys
+      return { ...state, ..._.mapKeys(action.paylod, 'id') }
+      break;
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload }
+      break;
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload }
+      break;
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload }
+      break;
+    case DELETE_STREAM:
+      // omit keeps the original state and creates a new one
+      _.omit(state, action.payload)
+      break;
+    default:
+      return state;
+  }
+}

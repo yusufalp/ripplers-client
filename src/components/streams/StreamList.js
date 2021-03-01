8a9acf03e06d370938d9dd6e6223992f30eaaae6
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-01 07:00:24 -0800

    add control buttons and render them if authentication passed

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
+  }
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      )
+    }
   }
   renderList() {
     return this.props.streams.map(stream => {
@@ -12,18 +23,38 @@ class StreamList extends React.Component {
         <div className="item" key={stream.id}>
           <p>{stream.title}</p>
           <p>{stream.description}</p>
+          <div>{this.renderAdmin(stream)}</div>
         </div>
       )
     })
   }
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div>
+          <Link to="/streams/new">
+            Create New Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
-    return <div>{this.renderList()}</div>
+    return (
+      <>
+        <div>{this.renderList()}</div>
+        <div>{this.renderCreate()}</div>
+      </>
+    )
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    streams: Object.values(state.streams)
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   }
 }
 

0759150292c5bdb831a57321f443478983806c0c
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-01 06:59:15 -0800

    add userId sections to create streams action

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,36 +23,38 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => {
-  return async (dispatch) => {
-    const response = await streams.post('/streams', formValues);
-    dispatch({ type: CREATE_STREAM, payload: response.data })
-  }
-}
+  return async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId });
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+
+  };
+};
 
 export const fetchStreams = () => {
   return async (dispatch) => {
     const response = await streams.get('/streams');
-    dispatch({ type: FETCH_STREAMS, payload: response.data })
-  }
-}
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+  };
+};
 
 export const fetchStream = (id) => {
   return async (dispatch) => {
     const response = await streams.get(`/streams/${id}`);
-    dispatch({ type: FETCH_STREAM, payload: response.data })
-  }
-}
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+  };
+};
 
 export const editStream = (id, formValues) => {
   return async (dispatch) => {
     const response = streams.put(`/streams/${id}`, formValues);
-    dispatch({ type: EDIT_STREAM, payload: response.data })
-  }
-}
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+  };
+};
 
 export const deleteStream = (id) => {
   return async (dispatch) => {
     await streams.delete(`/streams/${id}`);
-    dispatch({ type: DELETE_STREAM, payload: id })
-  }
-}
+    dispatch({ type: DELETE_STREAM, payload: id });
+  };
+};

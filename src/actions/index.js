1f5eaa4325b4d9381fcd4f209a73e8e66d72a582
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-05 07:05:30 -0800

    fixed by adding async/await

--- src/actions/index.js
+++ src/actions/index.js
@@ -48,7 +48,7 @@ export const fetchStream = (id) => {
 
 export const editStream = (id, formValues) => {
   return async (dispatch) => {
-    const response = streams.patch(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
     dispatch({ type: EDIT_STREAM, payload: response.data });
   };
 };

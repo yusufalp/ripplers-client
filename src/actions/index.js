44a3f9b14d22462abbe35769d8d20172aadb2f1a
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-05 08:55:24 -0800

    navigate to the home page after deletion

--- src/actions/index.js
+++ src/actions/index.js
@@ -57,5 +57,6 @@ export const deleteStream = (id) => {
   return async (dispatch) => {
     await streams.delete(`/streams/${id}`);
     dispatch({ type: DELETE_STREAM, payload: id });
+    history.push('/')
   };
 };
